4e4a6e0745428687e6b86466af311e9fbba50755
DAIGA <dayaxiqi@gmail.com>
2021-03-17 13:56:27 +0900

    その他

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+         has_many :messages, through: :room_users
+         has_many :user_messages
+
 end
