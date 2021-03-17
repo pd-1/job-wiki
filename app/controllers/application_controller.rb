65be725b9d480429479dd43aaeb290fdee3cf993
DAIGA <dayaxiqi@gmail.com>
2021-03-17 12:00:36 +0900

    ログイン後のしてパスへの遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "ログインに成功しました" 
+      jobs_path  # 指定したいパスに変更
+    else
+      flash[:notice] = "新規登録完了しました。" 
+      jobs_path  # 指定したいパスに変更
+    end
+  end
 end
