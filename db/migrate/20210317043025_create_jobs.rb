03a24044a62db7c01213a9043ef1ad684b2dfa94
DAIGA <dayaxiqi@gmail.com>
2021-03-18 12:55:05 +0900

    マイグレーションの追加と変更

--- db/migrate/20210317043025_create_jobs.rb
+++ db/migrate/20210317043025_create_jobs.rb
@@ -5,6 +5,8 @@ class CreateJobs < ActiveRecord::Migration[6.0]
       t.timestamps
       t.string :name,  null: false
       t.text :content,  null: false
+      t.references :user,     foreign_key: true 
+
     end
   end
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserMessageTest < ActiveSupport::TestCase
+class RoomTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
