be2054291ad1064e70db8548b90e9f90d679736e
DAIGA <dayaxiqi@gmail.com>
2021-04-02 15:23:41 +0900

    マイグレーションの設定

--- db/migrate/20210317043025_create_jobs.rb
+++ db/migrate/20210317043025_create_jobs.rb
@@ -4,7 +4,20 @@ class CreateJobs < ActiveRecord::Migration[6.0]
 
       t.timestamps
       t.string :name,  null: false
-      t.text :content,  null: false
+      t.integer :genre_id, null: false
+      t.text :description,  null: false 
+      t.text :active,  null: false 
+      t.text :flow,  null: false 
+      t.text :become,  null: false 
+      t.text :qualification,  null: false 
+      t.text :salary,  null: false 
+      t.text :rewarding,  null: false 
+      t.text :busy,  null: false 
+      t.text :correct,  null: false 
+      t.text :aspiring,  null: false 
+      t.text :status,  null: false 
+      t.text :demand,  null: false 
+      t.text :future,  null: false 
       t.references :user,     foreign_key: true 
 
     end
