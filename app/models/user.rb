f8bc9339036b9ac77bd79b5b11dbb665a370c754
DAIGA <dayaxiqi@gmail.com>
2021-04-09 15:18:38 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,15 @@ class User < ApplicationRecord
 
          has_many :chats
          has_many :messages
+         belongs_to :category
          has_one_attached :image
          with_options presence: true do
           validates :name
+<<<<<<< Updated upstream
           validates :genre_id, numericality: { other_than: 1 }
+=======
+          validates :category_id
+>>>>>>> Stashed changes
          end 
          extend ActiveHash::Associations::ActiveRecordExtensions
          belongs_to_active_hash :genre
