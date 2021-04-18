dcf20a73746be651b2ffc21a960996cedef76062
DAIGA <dayaxiqi@gmail.com>
2021-04-18 22:51:35 +0900

    ゲストユーザー

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get '/search', to: 'users/registrations#search',defaults: { format: 'json' }
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   
   resources :jobs
