53200ffbe9f52acfb91a65ba6d6d6610ed538e51
DAIGA <dayaxiqi@gmail.com>
2021-04-18 22:02:50 +0900

    ゲストログインの編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,8 @@ class HomeController < ApplicationController
 
   def new_guest
     user = User.find_by(email: 'guest@example.com') do |user|
-        user.password = 'SecureRandom.urlsafe_base64'
-        user.name = "ゲストユーザー"
-        user.category_id = 17
-    end
     sign_in user
-    redirect_to jobs_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to jobs_path
   end
 
 end
