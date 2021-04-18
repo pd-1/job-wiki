cbe347ea709d2f8b46e7e6b862bdb71b0ed55e68
DAIGA <dayaxiqi@gmail.com>
2021-04-18 18:54:56 +0900

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
   def new_guest
     user = User.find_or_create_by(email: 'guest@example.com') do |user|
       user.password = 'SecureRandom.urlsafe_base64'
+      user.email = "guest@example.com"
       user.name = "ゲストユーザー"
       user.category_id = 17
     end
