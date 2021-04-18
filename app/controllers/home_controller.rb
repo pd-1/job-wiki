6fa398d12bb858b91cea45cff32dd032512874bc
DAIGA <dayaxiqi@gmail.com>
2021-04-19 00:26:20 +0900

    簡単ログイン

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,12 @@ class HomeController < ApplicationController
   end
 
   def new_guest
-    user = User.find_by(email: 'guest@example.com')
-    sign_in user
-    redirect_to jobs_path
+   if user = User.find_by(email: 'guest@example.com')
+      sign_in user
+      redirect_to jobs_path
+   else
+      redirect_to root_path
+   end
   end
 
 end
