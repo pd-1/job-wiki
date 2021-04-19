d4294e31aff4743b49bc8231d631d7d73a2f6941
DAIGA <dayaxiqi@gmail.com>
2021-04-19 14:28:18 +0900

    ゲストユーザーの編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,17 @@ class HomeController < ApplicationController
   def index
   end
 
-  def new_guest
+  def new_guest1
    if user = User.find_by(email: 'guest@example.com')
+      sign_in user
+      redirect_to jobs_path
+   else
+      redirect_to root_path
+   end
+  end
+
+  def new_guest2
+   if user = User.find_by(email: 'guest2@example.com')
       sign_in user
       redirect_to jobs_path
    else
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get '/search', to: 'users/registrations#search',defaults: { format: 'json' }
+    post '/home/guest1_sign_in', to: 'home#new_guest1'
+    post '/home/guest2_sign_in', to: 'home#new_guest2'
   end
   
   resources :jobs
   resources :users, only:[:show,:edit,:update]
   resources :rooms, only:[:create, :show,:index]
   resources :messages, only:[:create,:destroy]
-  post '/home/guest_sign_in', to: 'home#new_guest'
 end
